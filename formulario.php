969c1d18b3dab235e4938ab894b25a3111f2d7f5
Darwin Chamba <darwinchambaa@gmail.com>
2024-07-01 21:51:26 -0500

    formularios

---
+++ formulario.php
@@ -0,0 +1,10 @@
+<?php
+
+$name=$_GET['name'];
+$apellido=$_GET['apellido'];
+$curso =$_GET['radio'];
+
+echo 'hola mi nombre es: '.$name ." ".$apellido. '<br>';
+echo "has seleccionado " .$curso;
+
+?>
