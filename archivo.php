fd3d0e68c3f10ace0cdbefc855027624eb5a9908
Darwin Chamba <darwinchambaa@gmail.com>
2024-07-01 20:37:19 -0500

    funciones de arreglos

--- archivo.php
+++ archivo.php
@@ -1,6 +1,23 @@
 <?php 
 echo 'Funciones para los arrays';
 echo '<br>';
+$arreglo=[5,6,4,2,3];
+//imprimiendo un valor dada la posicion
+echo $arreglo[2] . '<br>';
+//pagregar valor en una posicion especifica
+$arreglo[0]=666;
+//agrega un nuevo valor al final del arreglo
+array_push($arreglo,555);
+//elimina el elemento del arreglo que se encunetra en la ultima posicion
+array_pop($arreglo);
+//agrega un nuevo elmento al inicio del arreglo
+array_unshift($arreglo,777);
+//elimina el primer elmento del arreglo
+array_shift($arreglo);
+
+foreach($arreglo as $valor){
+    echo $valor .' ';
+}
 
 
 ?>
